f304612635ad332e938109b60dd145590857e096
Borsodi Ádám <borsodi.dm@gmail.com>
2022-12-05 17:08:33 +0100

    Basic

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,11 +1,23 @@
 <template lang="">
   <div>
-    <input type="text" v-modal="todo" />
-    <p>{{ todo }}</p>
+    <input
+      type="text"
+      v-model="todo"
+      @change="addItem(todo)"
+      placeholder="Új feladat megadása"
+    />
+    <ul>
+      <li v-for="todo in todoList">{{ todo }}</li>
+    </ul>
   </div>
 </template>
 <script setup>
   import { ref } from 'vue';
-  const todo = ref('');
+  const todo = ref(null);
+  const todoList = ref([]);
+  function addItem(t) {
+    todoList.value.push(t);
+    todo.value = '';
+  }
 </script>
 <style lang=""></style>
