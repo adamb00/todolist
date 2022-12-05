158c457c52d284f6650a9fbd0909f59fd4125466
Borsodi Ádám <borsodi.dm@gmail.com>
2022-12-05 16:54:29 +0100

    Start file

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,11 @@
+<template lang="">
+  <div>
+    <input type="text" v-modal="todo" />
+    <p>{{ todo }}</p>
+  </div>
+</template>
+<script setup>
+  import { ref } from 'vue';
+  const todo = ref('');
+</script>
+<style lang=""></style>
